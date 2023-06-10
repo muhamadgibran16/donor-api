2bb9ed53329dcb4c9abb5e7927a53b7d879b5f4a
muhamadgibran16 <muhamadgibran2333@gmail.com>
2023-06-10 17:42:54 +0700

    adD TOKEN in news

--- newslatter-api/routers/index.js
+++ newslatter-api/routers/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const {Pagination} = require('../../utils/pagination')
 const news = require('../../newslatter-api/handlers/news')
-
+const {
+  verifyToken
+} = require('../../auth-api/middleware/auth')
 router.use(Pagination)
 
+/** News */
 router.post('/add-news', news.createNews)
-router.get('/get-news', news.getNews)
+router.get('/get-news', verifyToken, news.getNews)
 
 module.exports = router
